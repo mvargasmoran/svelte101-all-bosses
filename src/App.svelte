1085a2e117c4002f769923855049b4f1f4cc53ef
mvargasmoran <mvargasmoran@gmail.com>
2021-05-10 02:44:00 -0400

    Add styling tutorial
    https://svelte.dev/tutorial/styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,9 @@
-<script>
-  let imgprops = {
-    src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F2711%2F4238%2Fproducts%2FHTB1FgIjdA5E3KVjSZFCq6zuzXXap_1024x1024.jpg%3Fv%3D1561166990',
-    alt: 'A tiny keyboard dances',
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam alias molestiae debitis, iusto eaque provident maxime totam sequi dolore, ea minus quo voluptate porro repellat omnis sint nemo nam vero.</p>
+
+<style>
+  p {
+    color: orchid;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 2em;
   }
-  const {src} = imgprops;
-</script>
-<img {src} alt="{imgprops.alt}">
+</style>

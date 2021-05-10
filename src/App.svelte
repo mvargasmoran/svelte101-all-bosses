52a6fd642c3f2b5dc79501bf086960fcbbfa74da
mvargasmoran <mvargasmoran@gmail.com>
2021-05-10 03:20:17 -0400

    Add Html Tags tutorial
    https://svelte.dev/tutorial/html-tags

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,6 @@
 <script>
-
-import Nested from "./Nested.svelte";
-
+  let string = `This string contains some <strong>html!!!</strong>`;
 </script>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam alias molestiae debitis, iusto eaque provident maxime totam sequi dolore, ea minus quo voluptate porro repellat omnis sint nemo nam vero.</p>
-
-<style>
-  p {
-    color: orchid;
-    font-family: 'Courier New', Courier, monospace;
-    font-size: 2em;
-  }
-</style>
-
-<Nested/>
+<p>
+  {@html string}
+</p>

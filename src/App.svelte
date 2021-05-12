8d463523d026b7dfba77b46478213869b8e32af5
mvargasmoran <mvargasmoran@gmail.com>
2021-05-12 01:43:48 -0400

    Add updating arrays & objects tutorial
    https://svelte.dev/tutorial/updating-arrays-and-objects

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,24 @@
 <script>
-  let count = 0;
-  $: doubled = count * 2;
-  $: if ( count >= 10) {
-    console.log(`the count is ${count}`);
-    alert(`count is too damn high`.toUpperCase());
-    count = 9;
+  //let numbers = [1, 2, 3, 4];
+  let numbers = [1, 2];
+  function addNumberFugly1() {
+    // push doesn't trigger and update/reactivity
+    numbers.push(numbers.length + 1);
+    numbers = numbers; // this ugly trick solves the issue, as reactivity is triggered by assignment
   }
 
-
-  function handleClick() {
-    count += 1;
+  function addNumberTrickery() {
+    numbers = [...numbers, numbers.length + 1]; // this it's still a little trick like, but it gets the job done
   }
+
+  function addNumber() {
+    numbers[numbers.length] = numbers.length + 1;
+  }
+
+  $: sum = numbers.reduce((t, n) => t + n, 0);
 </script>
 
-<button on:click="{handleClick}">
-  Clicked {count} {count === 1 ? 'time' : 'times' }
+<p>{numbers.join(' + ')} = {sum}</p>
+<button on:click="{addNumber}">
+  Add a number
 </button>
-<p>{count} doubled is {doubled}</p>

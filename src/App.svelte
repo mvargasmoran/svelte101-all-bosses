afad02db4943bee5c33145c63431a54653ea0560
mvargasmoran <mvargasmoran@gmail.com>
2021-05-12 03:15:15 -0400

    Add each blocks tutorial
    https://svelte.dev/tutorial/each-blocks

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,33 @@
 <script>
-  let x = 7;
+  	let cats = [
+		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+	];
 </script>
 
-{#if x > 10}
-  <p>{x} is greater than 10</p>
-{:else if 5 > x}
-  <p>{x} is less than 5</p>
-{:else}
-  <p>{x} is between 5 and 10</p>
-{/if}
+<h1>The famous Cats of Youtube</h1>
+
+<h3>Regular usage</h3>
+<ul>
+  {#each cats as cat, index}
+    <li>
+      <a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
+        {index+1}: {cat.name}
+      </a>
+    </li>
+  {/each}
+</ul>
+
+
+<h3>Destructuring usage</h3>
+<ul>
+  {#each cats as {id, name}, index}
+    <li>
+      <a target="_blank" href="https://www.youtube.com/watch?v={id}">
+        {index+1}: {name}
+      </a>
+    </li>
+  {/each}
+</ul>
+

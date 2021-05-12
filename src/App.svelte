2bfcded023a4f4c1a473234dfcdec1190cf51787
mvargasmoran <mvargasmoran@gmail.com>
2021-05-12 02:39:17 -0400

    Add spread props tutorial
    https://svelte.dev/tutorial/spread-props

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,13 @@
 <script>
-  import Nested from './Nested.svelte';
+  import Info from './Info.svelte';
+
+  const pkg = {
+    name: 'svelte',
+    version: 3,
+    speed: 'blazing',
+    website: 'https://svelte.dev',
+  };
 </script>
 The nested component:
-<Nested answer={23} color={'orange'}/>
-<Nested/>
+<Info name={pkg.name} version={pkg.version} speed={pkg.speed} website={pkg.website}/>
+<Info {...pkg}/>

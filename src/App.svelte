49a377b6af00165c9b0b74497698bc990dc13fc8
mvargasmoran <mvargasmoran@gmail.com>
2021-05-14 03:11:44 -0400

    Add component events tutorial
    A bit weird to be doing that, but this allows for some nifty reuse of
    component code
    https://svelte.dev/tutorial/component-events

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script>
-  function handleClick() {
-    alert('clicked');
+  import Inner from "./Inner.svelte";
+
+  function handleMessage(e) {
+    alert(e.detail.text);
   }
 </script>
-
-<button on:click|once={handleClick}>
-  Click me
-</button>
+<Inner on:message={handleMessage}/>
 
---
+++ src/Inner.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  function sayHello() {
+    dispatch('message', {
+      text: 'Hello! 😌',
+    });
+  }
+</script>
+
+<button on:click={sayHello}>
+	Click to say anything the event dispatches
+</button>

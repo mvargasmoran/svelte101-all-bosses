a417543f1ac0d9bd4281f72316c91d32c16d2ddc
mvargasmoran <mvargasmoran@gmail.com>
2021-05-14 02:20:11 -0400

    Add dom events tutorial
    https://svelte.dev/tutorial/dom-events

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,16 @@
 <script>
-	async function getRandomNumber() {
-		const res = await fetch(`https://svelte.dev/tutorial/random-number`);
-		const text = await res.text();
+  let m = { x: 0, y: 0 };
 
-		if (res.ok) {
-			return text;
-		} else {
-			throw new Error(text);
-		}
-	}
-
-  let promise = getRandomNumber();
-	function handleClick() {
-		promise = getRandomNumber();
-	}
+  function handleMousemove(event) {
+    m.x = event.clientX;
+    m.y = event.clientY;
+  }
 </script>
-<h1>
-  Start by calling the calling the promise
-</h1>
-<button on:click={handleClick}>
-	Fetch for a random number
-</button>
 
-{#await promise}
-	<p>...waiting</p>
-{:then number}
-	<p>The number is {number}</p>
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
-
-<div style="border: 1px solid orchid;min-height: 30px;">
-  <p>
-    There's no need for to await, then, catch.
-    it can just be await then.
-  </p>
-{#await promise then value}
-	<p>the value is {value}</p>
-{/await}
+<div on:mousemove={handleMousemove}>
+  The mouse position is {m.x} x {m.y}
 </div>
 
+<style>
+  div { width: 100%; height: 100%; }
+</style>

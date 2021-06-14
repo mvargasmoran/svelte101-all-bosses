219a938719b12c216fc1ffd10a91f0ad0c7955f1
mvargasmoran <mvargasmoran@gmail.com>
2021-06-14 02:13:22 -0400

    Add bindings numeric inputs tutorial
    https://svelte.dev/tutorial/numeric-inputs

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,17 @@
 <script>
-  let name = 'world';
+  let a = 1;
+  let b = 2;
 </script>
 
-<input bind:value={name} />
-<h1>Hello {name}</h1>
+<label for="">
+  <input type=number bind:value={a} min=0 max=10/>
+  <input type=range bind:value={a} min=0 max=10/>
+</label>
 
+
+<label for="">
+  <input type=number bind:value={b} min=0 max=10/>
+  <input type=range bind:value={b} min=0 max=10/>
+</label>
+
+<p>{a} + {b} = {a + b}</p>
